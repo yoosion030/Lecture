74823ce886319d6a37f95a48d4ffaf8e5eb55409
유시온 <80191860+yoosion030@users.noreply.github.com>
2021-08-20 20:49:24 +0900

    Add UseState

--- CodingApple/ReactClass/blog/src/App.js
+++ CodingApple/ReactClass/blog/src/App.js
@@ -1,20 +1,34 @@
+import React,{ useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
+  let [title,titleRevise] = useState(['곧 방학 끝남', '내일 코로나 검사', '풍암동 맛집']);
+
   let posts = '풍암동 맛집';
-  let style = {
-    color:'green',
-    fontSize:'30px'
-  }
 
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={style}>개발 Blog</div>
+        <div>개발 Blog</div>
       </div>
-      <h4>{posts}</h4>
+      <div className="list">
+        <h1>{title[0]}</h1>
+        <p>8월 20일</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h1>{title[1]}</h1>
+        <p>8월 20일</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h1>{title[2]}</h1>
+        <p>8월 20일</p>
+        <hr />
+      </div>
+      
     </div>
   );
 }
