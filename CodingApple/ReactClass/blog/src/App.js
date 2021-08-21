a3f419b4da22cdf65a131eac653c22d07bc7727b
유시온 <80191860+yoosion030@users.noreply.github.com>
2021-08-21 23:02:09 +0900

    Add Modal

--- CodingApple/ReactClass/blog/src/App.js
+++ CodingApple/ReactClass/blog/src/App.js
@@ -2,42 +2,58 @@ import React,{ useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+  function App() {
 
-  let [title,titleRevise] = useState(['곧 방학 끝남', '내일 코로나 검사', '풍암동 맛집']);
-  let [like,likeRevise] = useState(0);
+    let [title,titleRevise] = useState(['곧 방학 끝남', '내일 코로나 검사', '풍암동 맛집']);
+    let [like,likeRevise] = useState(0);
 
+    function changeTitle(){
+      var newTitle = [...title];
+      newTitle[0] = '와 신난다'
+      titleRevise( newTitle );
+    }
+    return (
+      <div className="App">
+        <div className="black-nav">
+          <div>개발 Blog</div>
+        </div>
+            <button onClick = { changeTitle }>버튼</button>
+        <div className="list">
+          <h3>
+            {title[0]} 
+            <span className="like" 
+                onClick = { ()=> {
+                likeRevise(like+1);
+            }}>👍</span>
+            {like}
+          </h3>
+          <p>8월 20일</p>
+          <hr />
+        </div>
+        <div className="list">
+          <h3>{title[1]} <span>👍</span></h3>
+          <p>8월 20일</p>
+          <hr />
+        </div>
+        <div className="list">
+          <h3>{title[2]}</h3>
+          <p>8월 20일</p>
+          <hr />
+        </div>
 
-  return (
-    <div className="App">
-      <div className="black-nav">
-        <div>개발 Blog</div>
+        <Modal></Modal>
       </div>
-      <div className="list">
-        <h3>
-          {title[0]} 
-          <span className="like" 
-              onClick = { ()=> {
-              likeRevise(like+1);
-          }}>👍</span>
-          {like}
-        </h3>
-        <p>8월 20일</p>
-        <hr />
-      </div>
-      <div className="list">
-        <h3>{title[1]} <span>👍</span> </h3>
-        <p>8월 20일</p>
-        <hr />
-      </div>
-      <div className="list">
-        <h3>{title[2]}</h3>
-        <p>8월 20일</p>
-        <hr />
-      </div>
-      
-    </div>
-  );
-}
+    );
+  }
+  function Modal(){
+    return(
+        <div className="modal">
+          <h2>제목</h2>
+          <p>날짜</p>
+          <p>상세 내용</p>
+        </div>
+    );
+  }
+
 
 export default App;
