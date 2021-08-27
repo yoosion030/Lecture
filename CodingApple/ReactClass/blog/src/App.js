4d1bb704f38f5509005e4d286a9f49d0310fe802
유시온 <80191860+yoosion030@users.noreply.github.com>
2021-08-28 00:26:54 +0900

    Add MapFunction

--- CodingApple/ReactClass/blog/src/App.js
+++ CodingApple/ReactClass/blog/src/App.js
@@ -5,46 +5,36 @@ import './App.css';
   function App() {
 
     let [title,titleRevise] = useState(['곧 방학 끝남', '내일 코로나 검사', '풍암동 맛집']);
-    let [like,likeRevise] = useState(0);
+    let [like,likeRevise] = useState([0]);
     let [modal,modalRevise] = useState(false);
-    let onOff = false;
-    
-    function changeTitle(){
-      var newTitle = [...title];
-      newTitle[0] = '와 신난다'
-      titleRevise( newTitle );
-    }
+        
+    // function changeTitle(){
+    //   var newTitle = [...title];
+    //   newTitle[0] = '와 신난다'
+    //   titleRevise( newTitle );
+    // }
 
     return (
       <div className="App">
         <div className="black-nav">
           <div>개발 Blog</div>
         </div>
-        <div className="list">
-          <h3>
-            {title[0]} 
-            <span className="like" 
-                onClick = { ()=> {
-                likeRevise(like+1);
-            }}>👍</span>
-            {like}
-          </h3>
-          <p>8월 20일</p>
-          <hr />
-        </div>
-        <div className="list">
-          <h3>{title[1]} <span>👍</span></h3>
-          <p>8월 20일</p>
-          <hr />
-        </div>
-        <div className="list">
-          <h3>{title[2]}</h3>
-          <p>8월 20일</p>
-          <hr />
-        </div>
-
+        {
+          title.map(function(a){
+            return (
+            <div className="list">
+              <h3>
+                {a}
+                <span className="like" onClick = { ()=> { likeRevise(like+1);}}>👍</span>
+                {like}
+              </h3>
+              <p>8월 20일</p>
+              <hr />
+            </div>
+            )
+          })
+        }
         <button onClick = {()=>{modalRevise(!modal)}}>버튼</button>
-
         {
           modal === true 
           ? <Modal></Modal>
