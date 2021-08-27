4f247be709280d5fa570d8982ee0b8021c0c9040
유시온 <80191860+yoosion030@users.noreply.github.com>
2021-08-28 00:00:09 +0900

    Add Modal

--- CodingApple/ReactClass/blog/src/App.js
+++ CodingApple/ReactClass/blog/src/App.js
@@ -6,18 +6,30 @@ import './App.css';
 
     let [title,titleRevise] = useState(['곧 방학 끝남', '내일 코로나 검사', '풍암동 맛집']);
     let [like,likeRevise] = useState(0);
-
+    let [modal,modalRevise] = useState(false);
+    let onOff = false;
+    let a;
+    
     function changeTitle(){
       var newTitle = [...title];
       newTitle[0] = '와 신난다'
       titleRevise( newTitle );
     }
+    function modalSwitch(){
+      if(onOff === false){
+        onOff = true;
+      } else {
+        onOff = false;
+      }
+      console.log(onOff)
+      modalRevise(onOff);
+    }
+
     return (
       <div className="App">
         <div className="black-nav">
           <div>개발 Blog</div>
         </div>
-            <button onClick = { changeTitle }>버튼</button>
         <div className="list">
           <h3>
             {title[0]} 
@@ -41,7 +53,12 @@ import './App.css';
           <hr />
         </div>
 
-        <Modal></Modal>
+        <button onClick = {modalSwitch}>버튼</button>
+        {
+          onOff === true 
+          ? <Modal></Modal>
+          : null
+        }
       </div>
     );
   }
