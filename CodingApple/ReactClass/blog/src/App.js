9f9740a133c3ad7024df80ace226172e60cf8dd7
유시온 <80191860+yoosion030@users.noreply.github.com>
2021-08-28 14:54:14 +0900

    Add Props

--- CodingApple/ReactClass/blog/src/App.js
+++ CodingApple/ReactClass/blog/src/App.js
@@ -5,48 +5,54 @@ import './App.css';
   function App() {
 
     let [title,titleRevise] = useState(['곧 방학 끝남', '내일 코로나 검사', '풍암동 맛집']);
-    let [like,likeRevise] = useState([0]);
+    let [like,likeRevise] = useState([0,1,0]);
     let [modal,modalRevise] = useState(false);
+    let [number,numberRevise] = useState(0);
         
     // function changeTitle(){
     //   var newTitle = [...title];
     //   newTitle[0] = '와 신난다'
     //   titleRevise( newTitle );
     // }
-
+    function changeLike(i){
+      let newLike = [...like];
+      newLike[i]+=1;
+      likeRevise(newLike);
+    }
     return (
       <div className="App">
         <div className="black-nav">
           <div>개발 Blog</div>
         </div>
         {
-          title.map(function(a){
+          title.map(function(a, i){
             return (
             <div className="list">
-              <h3>
+              <h3 onClick={()=>{numberRevise(i)}}>
                 {a}
-                <span className="like" onClick = { ()=> { likeRevise(like+1);}}>👍</span>
-                {like}
+                <span className="like" onClick = { ()=> {changeLike(i)}}>👍</span>
+                {like[i]}
               </h3>
               <p>8월 20일</p>
               <hr />
             </div>
-            )
+            );
           })
         }
+
         <button onClick = {()=>{modalRevise(!modal)}}>버튼</button>
         {
           modal === true 
-          ? <Modal></Modal>
+          ? <Modal title={title} number={number}></Modal>
           : null
         }
       </div>
     );
   }
-  function Modal(){
+  function Modal(props){
     return(
         <div className="modal">
-          <h2>제목</h2>
+          <h2>{ props.title[props.number] }</h2>
           <p>날짜</p>
           <p>상세 내용</p>
         </div>
