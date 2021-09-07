94d0a00b226413894f9cda2cf94a4c38ef8659bb
yoosion <rsy5487@naver.com>
2021-09-07 20:30:46 +0900

    reset

--- NomadCoder/momentum/src/momentum.js
+++ NomadCoder/momentum/src/momentum.js
@@ -1,14 +1,6 @@
 const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const greeting = document.getElementById("greeting");
+const loginButton = loginForm.querySelector("button");
 
-function SubmitClick(event){
-    event.preventDefault();
-    const username = loginInput.value;
-    localStorage.setItem("username", username);
-    loginForm.classList.add("hidden");
-    console.log(username);
-    greeting.innerText = `Hello ${username}`;// "Hello " + username;
-    greeting.classList.remove("hidden");
-}
-loginForm.addEventListener("submit",SubmitClick);
+loginForm.addEventListener("submit",()=> {
+    
+})
